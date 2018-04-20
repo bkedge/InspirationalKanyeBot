ac1bae9ccd0fdca3a7d3c451e38cc7f16f346fef
Brady Kedge <bradykedge@gmail.com>
2018-04-20 13:14:00 -0500

    Added ignoring links and long tweets

--- src/kanyebot.py
+++ src/kanyebot.py
@@ -6,6 +6,7 @@ import tweepy
 import time, random, os
 import re
 import textwrap
+from time import ctime
 from PIL import Image, ImageDraw, ImageFont
 from PIL import ImageFile
 from config import *
@@ -31,7 +32,7 @@ def getTweet():
 
     #Get most recent tweet
     recentTweet = api.user_timeline('kanyewest')[0]
-    print("Getting last tweet\n")
+    print("Getting last tweet at %s\n" % time.ctime())
 
     #If tweet is new make a new picture
     if recentTweet != lastTweet:
@@ -44,9 +45,24 @@ def getTweet():
 #Makes picture
 def makePic(newTweet):
     txt = newTweet.text
+
+    #check for link. If so ignore
+    regex = r"(http|https)"
+
+    if re.search(regex, txt, re.IGNORECASE):
+        print("Tweet is a link, skipping\n")
+        return
+
+    #Dont want paragraph tweets
+    if len(txt) > 50:
+        print("Tweet is too long to look good\n")
+        return
+
     txt.upper()
     print("Photo text is: " + newTweet.text + "\n")
     path = r"/home/brady/Code/Python/Twitter/KanyeBot/img"
+
+    
 
     #Get pictures
     pic = random.choice(os.listdir(path))
@@ -66,19 +82,34 @@ def makePic(newTweet):
         draw = ImageDraw.Draw(temp)
         width, height = temp.size
         textWidth, textHeight = draw.textsize(txt, font = font)
-
+        
         #Setup wrapping
-        lines = textwrap.wrap(txt, width = 40)
+        #lines = textwrap.wrap(txt, width = 40)
+        #current_h = 50
+        #pad = 10
 
         #Border
-        #draw.text((((width - textWidth)/2)-1, ((height-textHeight)/2)-1), txt, font = font, fill = "black")
-        #draw.text((((width - textWidth)/2)+1, ((height-textHeight)/2)-1), txt, font = font, fill = "black")
-        #draw.text((((width - textWidth)/2)-1, ((height-textHeight)/2)+1), txt, font = font, fill = "black")
-        #draw.text((((width - textWidth)/2)+1, ((height-textHeight)/2)+1), txt, font = font, fill = "black")
+
+        #Old style for border. May not be applicable with wrapping text. Need to find another way
+        draw.text((((width - textWidth)/2)-1, ((height-textHeight)/2)-1), txt, font = font, fill = "black")
+        draw.text((((width - textWidth)/2)+1, ((height-textHeight)/2)-1), txt, font = font, fill = "black")
+        draw.text((((width - textWidth)/2)-1, ((height-textHeight)/2)+1), txt, font = font, fill = "black")
+        draw.text((((width - textWidth)/2)+1, ((height-textHeight)/2)+1), txt, font = font, fill = "black")
 
         #Draw Text
+        draw.text(((width - textWidth)/2, (height-textHeight)/2), txt, font = font, fill = "white")
+
+        '''
         for line in lines:
-            draw.text(((width - textWidth)/2, (height-textHeight)/2), txt, font = font, fill = "white")
+            textWidth, textHeight = draw.textsize(line, font = font)
+            #Old way
+            #draw.text(((width - textWidth)/2, (height-textHeight)/2), txt, font = font, fill = "white")
+            draw.text(((width - textWidth)/2, (height-textHeight)/2), line, font = font, fill = "white")
+            
+            #Draw the text
+            #draw.text(((width - textWidth)/2, current_h), line, font = font, fill = "white")
+            #current_h += textHeight + pad
+        '''
 
         #Save it
         temp.save('/home/brady/Code/Python/Twitter/KanyeBot/img/final.jpg')
@@ -86,7 +117,7 @@ def makePic(newTweet):
         #Upload picture
         api.update_with_media('/home/brady/Code/Python/Twitter/KanyeBot/img/final.jpg')
         print("Posted tweet\n")
-    except Exception, e:
+    except Exception as e:
         print(e)
 
 if __name__ == "__main__":
@@ -95,5 +126,5 @@ if __name__ == "__main__":
             #Try stuff
             getTweet()
             time.sleep(600)
-        except Exception, e:
+        except Exception as e:
             print(e)
